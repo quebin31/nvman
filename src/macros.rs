75258813874bae2d2925900cf043be3a68d41924
Kevin <quebin31@gmail.com>
2019-07-20 21:14:32 -0500

    Don't use sudo if current user is root

--- src/macros.rs
+++ src/macros.rs
@@ -3,13 +3,19 @@ macro_rules! sudo_bash {
     ($($cmd:tt)*) => {{
         use ansi_term::Colour::Green;
         use shells::{bash, execute_with};
+        use users::get_current_uid;
 
-        let (code, _, _) = bash!("sudo -n true");
-        if code == 1 {
-            println!("{} Privelege scalation needed", Green.paint("==>"));
+        if get_current_uid() != 0 {
+            let (code, _, _) = bash!("sudo -n true");
+
+            if code != 0 {
+                println!("{} Privilege scalation needed", Green.paint("==>"));
+            }
+
+            execute_with("bash", &format!("sudo {}", format!($($cmd)*)))
+        } else {
+            execute_with("bash", &format!($($cmd)*))
         }
-
-        execute_with("bash", &format!("sudo {}", format!($($cmd)*)))
     }};
 }
 
