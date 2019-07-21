3f4a4ae9f48d7a5987de6855badaebd818cbb97f
Kevin <quebin31@gmail.com>
2019-07-20 21:15:01 -0500

    Replace clap, with manual method

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,258 @@
-use clap::clap_app;
-
 #[macro_use]
 mod macros;
-
+mod bumblebee;
 mod nvidia;
+mod optimus;
+mod settings;
 mod systemd;
 mod utils;
 
-fn main() {
-    let mut nvman = clap_app! { nvman =>
-        (version: "2.0")
-        (author: "Kevin D. <quebin31@gmail.com>")
-        (about: "Manage your NVIDIA powered laptop, the right way")
-        (@subcommand nvidia =>
-            (about: "Turn on/off your NVIDIA GPU, useful for CUDA (values: on, off, auto)")
-            (@subcommand on => 
-             (about: "Turn on your NVIDIA GPU"))
-            (@subcommand off => 
-             (about: "Turn off your NVIDIA GPU"))
-            (@subcommand auto => 
-             (about: "Toggle the state of your NVIDIA GPU")))
-            //(@arg state: +required "New GPU state (values: on, off, auto)"))
-        (@subcommand run =>
-            (about: "Run a program using primus-run")
-            (@arg program: +required ... "The program to be run, and its arguments"))
-        (@subcommand switch =>
-            (about: "Switch xsession GPU, this will log out you (values: auto, intel, nvidia)")
-            (@arg gpu: +required "Which GPU to use now (values: auto, intel, nvidia)"))
-        (@subcommand startup =>
-            (about: "Set the starting GPU when booting up (values: nvidia, intel, nvidia_once)")
-            (@arg gpu: +required "Which GPU to start at boot (values: nvidia, intel, nvidia_once)"))
-        (@subcommand default =>
-            (about: "Set a default service to start at boot (values: optimus, bumblebee)")
-            (@arg service: +required "The service to be started at boot (values: optimus, bumblebee)"))
-        (@subcommand status =>
-            (about: "Show the overall status (default subcommand)"))
-    };
+use shells::bash;
+use std::env;
+use std::process;
+use users::get_current_uid;
 
-    let matches = nvman
-        .clone()
-        .get_matches_safe()
-        .unwrap_or_else(|e| e.exit());
+use crate::systemd::Service;
 
+fn run_app() -> Result<(), ()> {
+    let args: Vec<_> = env::args().collect();
 
-    let nvman_service = systemd::Service::new("nvman");
-    if nvman_service.status().1 == systemd::Status::Disabled {
-        warn_print!("`nvman.service` is not enabled, potential risk!");
+    if get_current_uid() == 0 {
+        if let Some(cmd) = args.get(1) {
+            if cmd != "inspector" && cmd != "default" {
+                error_print!("Only `inspector` and `default` commands can be run as root!");
+                return Err(());
+            }
+        } else {
+            error_print!("Don\'t run this script as root!");
+            return Err(());
+        }
     }
 
-    match matches.subcommand() {
-        ("status", _) | ("", _) => {
+    utils::checks::check_nvman_service();
+    if args.len() == 1 {
+        utils::print::print_status();
+        return Ok(());
+    }
+
+    match args[1].as_str() {
+        "nvidia" => {
+            if utils::checks::check_optimus_mode().is_bad() {
+                return Err(());
+            }
+
+            if let Some(arg) = args.get(2) {
+                match arg.as_str() {
+                    "on" => nvidia::set_state(nvidia::State::On),
+                    "off" => nvidia::set_state(nvidia::State::Off),
+                    "auto" => nvidia::toggle_state(),
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_nvidia();
+                        return Err(());
+                    }
+                }
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_nvidia();
+                return Err(());
+            }
+        }
+
+        "run" => {
+            if utils::checks::check_optimus_mode().is_bad() {
+                return Err(());
+            }
+
+            if let Some(arg) = args.get(2) {
+                bumblebee::run(arg, &args[3..]);
+            } else {
+                error_print!("Please provide a command to run!");
+                utils::print::print_usage_run();
+                return Err(());
+            }
+        }
+
+        "switch" => {
+            if let Some(arg) = args.get(2) {
+                if utils::checks::check_gdm_prime().is_bad() {
+                    return Err(());
+                }
+
+                match arg.as_str() {
+                    "nvidia" => {
+                        info_print!("If your screen keeps black, switch across ttys");
+                        bash!("sleep 1");
+                        optimus::set_mode(optimus::Mode::Nvidia)
+                    }
+
+                    "intel" => {
+                        info_print!("If your screen keeps black, switch across ttys");
+                        bash!("sleep 1");
+                        optimus::set_mode(optimus::Mode::Intel)
+                    }
+
+                    "auto" => {
+                        info_print!("If your screen keeps black, switch across ttys");
+                        bash!("sleep 1");
+                        optimus::toggle_mode()
+                    }
+
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_switch();
+                        return Err(());
+                    }
+                }
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_switch();
+                return Err(());
+            }
+        }
+
+        "startup" => {
+            if let Some(arg) = args.get(2) {
+                match arg.as_str() {
+                    "nvidia" => optimus::set_startup(optimus::Mode::Nvidia),
+                    "intel" => optimus::set_startup(optimus::Mode::Intel),
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_startup();
+                        return Err(());
+                    }
+                }
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_startup();
+                return Err(());
+            }
+        }
+
+        "default" => {
+            if let Some(arg) = args.get(2) {
+                if get_current_uid() != 0 {
+                    let (code, stdout, _) =
+                        sudo_bash!("{} default {}", std::env::args().nth(0).unwrap(), arg);
+
+                    print!("{}", stdout);
+                    if code != 0 {
+                        return Err(());
+                    } else {
+                        return Ok(());
+                    }
+                }
+
+                match arg.as_str() {
+                    "optimus" => {
+                        info_print!("Setting {} as default service", arg);
+                        settings::set_default_service("optimus")
+                    }
+
+                    "bumblebee" => {
+                        info_print!("Setting {} as default service", arg);
+                        settings::set_default_service("bumblebee")
+                    }
+
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_default();
+                        return Err(());
+                    }
+                }
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_default();
+                return Err(());
+            }
+        }
+
+        "start" => {
+            if let Some(arg) = args.get(2) {
+                let optimus = Service::new("optimus-manager");
+                let bumblebee = Service::new("bumblebeed");
+
+                match arg.as_str() {
+                    "optimus" => {
+                        bumblebee.stop();
+                        optimus.start();
+                    }
+
+                    "bumblebee" => {
+                        optimus.stop();
+                        bumblebee.start();
+                    }
+
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_start();
+                        return Err(());
+                    }
+                }
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_start();
+                return Err(());
+            }
+        }
+
+        "stop" => {
+            if let Some(arg) = args.get(2) {
+                let optimus = Service::new("optimus-manager");
+                let bumblebee = Service::new("bumblebeed");
+
+                match arg.as_str() {
+                    "optimus" => optimus.stop(),
+                    "bumblebee" => bumblebee.stop(),
+                    invalid => {
+                        error_print!("Invalid value `{}`", invalid);
+                        utils::print::print_usage_stop();
+                        return Err(());
+                    }
+                };
+            } else {
+                error_print!("Please provide a value!");
+                utils::print::print_usage_stop();
+                return Err(());
+            }
+        }
+
+        "inspector" => {
+            let (default, non_default) = match settings::get_default_service().as_str() {
+                "optimus" => (Service::new("optimus-manager"), Service::new("bumblebeed")),
+                "bumblebee" => (Service::new("bumblebeed"), Service::new("optimus-manager")),
+                _ => unreachable!(),
+            };
+
+            if non_default.enabled() {
+                non_default.disable();
+            }
+
+            if !default.enabled() {
+                default.enable();
+            }
+        }
+
+        "status" => {
             utils::print::print_status();
         }
 
-        ("nvidia", Some(sub)) => {
-            let state = sub.value_of("state").unwrap();
-
-            match state.to_lowercase().as_str() {
-                "on" => nvidia::set_state(nvidia::State::On),
-                "off" => nvidia::set_state(nvidia::State::Off),
-                "auto" =>  nvidia::toggle_state(),
-                invalid => {
-                    error_print!("Invalid value `{}`", invalid);
-                    println!("{} {}", sub.usage(), "(state: on, off, auto)");
-                }
-            }
+        "help" => {
+            utils::print::print_help();
         }
 
-        ("startup", Some(sub)) => {
-            let startup = sub.value_of("gpu").unwrap();
-            match startup.to_lowercase().as_str() {
-                "nvidia" => utils::optimus::set_startup(utils::optimus::Mode::Nvidia),
-                "intel" => utils::optimus::set_startup(utils::optimus::Mode::Intel),
-                invalid => {
-                    error_print!("Invalid value `{}`", invalid);
-                    println!("{} {}", sub.usage(), "(gpu: nvidia, intel)");
-                }
-            }
+        invalid_command => {
+            error_print!("Invalid command `{}`", invalid_command);
+            utils::print::print_help();
         }
-
-        ("switch", Some(sub)) => {
-            unimplemented!()
-        }
-
-        ("run", Some(sub)) => {
-            unimplemented!()
-        }
-        _ => unreachable!(),
     }
+
+    return Ok(());
+}
+
+fn main() -> Result<(), ()> {
+    process::exit(match run_app() {
+        Ok(_) => 0,
+        Err(_) => 1,
+    });
 }
