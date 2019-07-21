a171d0961f92988a2d4fe3130e736da11693f736
Kevin <quebin31@gmail.com>
2019-07-20 21:17:22 -0500

    Deleted ServiceStatus Working with boolean functions

--- src/systemd.rs
+++ src/systemd.rs
@@ -1,45 +1,7 @@
-use ansi_term::Colour::{Green, Red, Yellow};
-use shells::bash;
 use std::fmt;
 
-#[derive(Debug, PartialEq)]
-pub enum Status {
-    Enabled,
-    Disabled,
-    Active,
-    Inactive,
-}
-
-impl fmt::Display for Status {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let status = match self {
-            Status::Enabled => "(enabled)",
-            Status::Disabled => "(disabled)",
-            Status::Active => "active",
-            Status::Inactive => "inactive",
-        };
-
-        write!(f, "{}", status)
-    }
-}
-
-#[derive(Debug)]
-pub struct ServiceStatus(pub Status, pub Status);
-
-impl fmt::Display for ServiceStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let acolor = match self.0 {
-            Status::Active => Green.normal(),
-            Status::Inactive => Red.normal(),
-            _ => unreachable!(),
-        };
-
-        let activeness = acolor.paint(format!("{:<8}", self.0.to_string()));
-        let enableness = Yellow.paint(format!("{:<10}", self.1.to_string()));
-
-        write!(f, "{} {}", activeness, enableness)
-    }
-}
+use shells::bash;
+use ansi_term::Colour::{Green, Red, Yellow};
 
 #[derive(Debug)]
 pub struct Service {
@@ -53,22 +15,16 @@ impl Service {
         }
     }
 
-    pub fn status(&self) -> ServiceStatus {
-        let (_code, stdout, _) = bash!("/usr/bin/systemctl status {}", self.descriptor);
+    pub fn enabled(&self) -> bool {
+        let (_, stdout, _) = bash!("/usr/bin/systemctl status {}", self.descriptor);
 
-        let activeness = if stdout.contains("inactive") {
-            Status::Inactive
-        } else {
-            Status::Active
-        };
+        stdout.contains("enabled;")
+    }
 
-        let enableness = if stdout.contains("enabled;") {
-            Status::Enabled
-        } else {
-            Status::Disabled
-        };
+    pub fn active(&self) -> bool {
+        let (_, stdout, _) = bash!("/usr/bin/systemctl status {}", self.descriptor);
 
-        ServiceStatus(activeness, enableness)
+        !stdout.contains("inactive")
     }
 
     pub fn enable(&self) -> bool {
@@ -89,5 +45,23 @@ impl Service {
     pub fn stop(&self) -> bool {
         let (code, _, _) = sudo_bash!("/usr/bin/systemctl stop {}", self.descriptor);
         code == 0
+    }
+}
+
+impl fmt::Display for Service {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let activeness = if self.active() {
+            Green.paint(format!("{:<8}", "active"))
+        } else {
+            Red.paint(format!("{:<8}", "inactive"))
+        };
+
+        let enableness = if self.enabled() {
+            Yellow.bold().paint(format!("{:<10}", "(enabled)"))
+        } else {
+            Yellow.paint(format!("{:<10}", "(disabled)"))
+        };
+
+        write!(f, "{} {}", activeness, enableness)
     }
 }
